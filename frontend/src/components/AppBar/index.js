0c0a7ce3a9c8b99f3349bc923498e9cffeab7926
Ana Lúcia Canto <ana.lucia.canto@poli.ufrj.br>
2022-08-03 19:53:18 -0300

    Adding AppBar component

---
+++ frontend/src/components/AppBar/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+
+
+const TopBar = () => {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>       
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default TopBar;
